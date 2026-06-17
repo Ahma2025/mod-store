771f37c5832163b1b22c6c3fda8b29b18d740ca7
Ahma2025 <engahmadjamall00@gmail.com>
2026-06-17 16:39:26 +0300

    fix: add GoogleService-Info.plist to Xcode target + fix AppDelegate return

--- glamora-salon/ios/App/App/AppDelegate.swift
+++ glamora-salon/ios/App/App/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
         nativeDebug("App launched - Firebase configured")
-        return ApplicationDelegateProxy.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return true
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
